c8c673cbcab5192b18bdedbb990b519dd2934d02
Punnd <521h0039@student.tdtu.edu.vn>
2023-12-03 23:26:36 +0700

    layout_login_register_home

---
+++ app/src/main/java/com/example/mid_term_mobile/list_students_view.java
@@ -0,0 +1,14 @@
+package com.example.mid_term_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_students_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_students_view);
+    }
+}
---
+++ app/src/main/java/com/example/mid_term_mobile/student_profile.java
@@ -0,0 +1,14 @@
+package com.example.mid_term_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class student_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_profile);
+    }
+}
